6927255af917eddf4612bdef025f04f6fd085677
barkas <vineda.contact@gmail.com>
2023-12-19 19:59:43 +0200

    feat: add Transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  
+  const result = arr.slice();
+  
+  for (let i = 0; i < result.length; i++) {
+    switch (result[i]) {
+      case '--discard-next':
+        if (i < (result.length - 1)) {
+          result.splice(i + 1, 1);
+        }
+        break;
+      case '--discard-prev':
+        if (i > 0) {
+          result.splice(i - 1, 1);
+          i--;
+        }
+        break;
+      case '--double-next':
+        if (i < result.length - 1) {
+          result.splice(i + 1, 0, result[i + 1]);
+        }
+        break;
+      case '--double-prev':
+        if (i > 0) {
+          result.splice(i, 0, result[i - 1]);
+          i++;
+        }
+        break;
+    }
+  }
+
+  const controls = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+  return result.filter((item) => !controls.includes(item));
 }
 
 module.exports = {
