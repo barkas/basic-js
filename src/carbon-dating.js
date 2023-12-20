baecb5f3fb21e32394598b0126f8cf7e519ce9ad
barkas <vineda.contact@gmail.com>
2023-12-20 18:28:16 +0200

    feat: add Carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (!sampleActivity ||
+    typeof(sampleActivity) !== 'string' ||
+    !Number(sampleActivity) ||
+    Number(sampleActivity) > MODERN_ACTIVITY ||
+    Number(sampleActivity) <= 0
+    ) {
+    return false;
+  }
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / k);
 }
 
 module.exports = {
